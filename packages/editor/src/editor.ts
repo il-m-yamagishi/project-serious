8bb5008d7a70457346f8867ed5919171d0348336
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2024-06-11 10:44:48 +0900

    feat: Selectable Gizmo using Z/X/C

--- packages/editor/src/editor.ts
+++ packages/editor/src/editor.ts
@@ -5,6 +5,7 @@
 import { createEngineAsync } from "@m-yamagishi/project-serious-core/src/createEngineAsync";
 import { EditorScene } from "./editor-scene";
 import { GizmoManager } from "@babylonjs/core/Gizmos/gizmoManager";
+import { KeyboardEventTypes } from "@babylonjs/core/Events/keyboardEvents";
 
 export class Editor {
   constructor(private readonly canvas: HTMLCanvasElement) {
@@ -16,9 +17,27 @@ export class Editor {
 
     const gizmoManager = new GizmoManager(scene);
     gizmoManager.positionGizmoEnabled = true;
-    // gizmoManager.rotationGizmoEnabled = true;
-    // gizmoManager.scaleGizmoEnabled = true;
     gizmoManager.attachableMeshes = scene.meshes;
+
+    scene.onKeyboardObservable.add((kbInfo) => {
+      if (kbInfo.type === KeyboardEventTypes.KEYDOWN) {
+        if (kbInfo.event.key === "z") {
+          gizmoManager.positionGizmoEnabled = true;
+          gizmoManager.rotationGizmoEnabled = false;
+          gizmoManager.scaleGizmoEnabled = false;
+        }
+        if (kbInfo.event.key === "x") {
+          gizmoManager.positionGizmoEnabled = false;
+          gizmoManager.rotationGizmoEnabled = true;
+          gizmoManager.scaleGizmoEnabled = false;
+        }
+        if (kbInfo.event.key === "c") {
+          gizmoManager.positionGizmoEnabled = false;
+          gizmoManager.rotationGizmoEnabled = false;
+          gizmoManager.scaleGizmoEnabled = true;
+        }
+      }
+    });
 
     const render = () => {
       scene.render();
